ab710465b0d1753465d8ec9fb5a57b2526ace9ab
Ruhin Shukurlu <ruhinshukurlu@gmail.com>
2023-07-14 12:06:06 +0400

    dynamic path segments

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("task1", views.task_one),
-    path("task2", views.task_two)
+    path("<month>", views.get_monthly_task),
 ]
--- core/views.py
+++ core/views.py
@@ -1,11 +1,24 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 
 
 
-def task_one(request):
-    return HttpResponse("Walk in the streen for an hour!")
+# def task_one(request):
+#     return HttpResponse("Walk in the streen for an hour!")
 
 
-def task_two(request):
-    return HttpResponse("Do another thing!")
+# def task_two(request):
+#     return HttpResponse("Do another thing!")
+
+
+def get_monthly_task(request, month):
+    task_text = None
+
+    if month == "january":
+        task_text = "Walk in the streen for an hour!"
+    elif month == "february":
+        task_text = "Do another thing!"
+    else:
+        return HttpResponseNotFound("This month isn't supported")
+
+    return HttpResponse(task_text)
